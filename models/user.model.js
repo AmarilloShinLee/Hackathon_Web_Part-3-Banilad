51ee491db04282f03014f5eef4d3bf1868edea03
Elijah Nicholas Esguerra <ashesg1117@gmail.com>
2023-05-23 13:53:46 +0800

    commited backend

---
+++ models/user.model.js
@@ -0,0 +1,66 @@
+// User Schema/Table. All of the user information is defined here
+
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+
+const userSchema = new schema({
+	firstname: {
+		type: String,
+		required: true,
+		unique: false,
+		trim: true,
+		minlength: 3
+	},
+	
+	middlename: {
+		type: String,
+		required: false, // not everyone has a middle name
+		unique: false,
+		trim: true,
+		mindlength: 3
+	},
+
+	lastname: {
+		type: String,
+		required: true,
+		unique: false,
+		trim: true,
+		minlength: 3
+	},
+
+	username: {
+		type: String,
+		required: true,
+		unique: true,
+		trim: true,
+		minlength: 3
+	},
+
+	birthdate: {
+		type: Date,
+		required: true,
+		unique: false,
+	},
+
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+		trim: true,
+		minlength: 10
+	},
+
+	password: {
+		type: String,
+		required: true,
+		unique: false,
+		trim: true,
+		minlength: 6
+	}
+});
+
+// assign this model to the mongoose models
+const User = mongoose.model('Users', userSchema);
+
+// export this model
+module.exports = User;
---
+++ routes/users.js
@@ -0,0 +1,36 @@
+const router = require('express').Router(); // use express router for this one
+let User = require('../models/user.model'); // assign temporary variables
+
+// login endpoint
+router.route('/login').get((req, res) => {
+
+	// view all lang sa sa tanan users
+	User.find()
+	.then(users => res.status(200).json(users))
+	.then(error => res.status(400).json('Error! ' + error));
+});
+
+// signup enpoint
+
+//Added another end point for sign up
+router.route('/sign_up/add').post((req, res) => {
+	
+	// igka send ug post request makuha ni	 sila dapat
+	const firstname = req.body.firstname;
+	const middlename = req.body.middlename;
+	const lastname = req.body.lastname;
+	const birthdate = req.body.birthdate;
+	const username = req.body.username;
+	const email = req.body.email;
+	const password = req.body.password;
+
+	// cast into a single object
+	const newUser = new User({firstname, middlename, lastname, username, birthdate, email, password});
+
+	// register user
+	newUser.save()
+		.then(() => res.status(200).json('Successfully registered'))
+		.catch((err) => res.status(400).json('Error in registration.' + err));
+});
+
+module.exports = router;
