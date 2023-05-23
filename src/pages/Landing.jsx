9099f44214647f5a15f03b7483c9631e71073aca
Alex <stellmarzie@gmail.com>
2023-05-23 13:48:10 +0800

    Added a change in landing page for testing purposes

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,14 +1,15 @@
-import Paper from '@mui/material/Paper'
-import Container from '@mui/material/Container'
+import Paper from "@mui/material/Paper";
+import Container from "@mui/material/Container";
 
 const LandingPage = () => {
-    return (
-        <Container maxWidth="xl">
-            <Paper elevation={4}  className='p-4' style={{marginTop: "16em"}}>
-                <h2>This is a Landing page!</h2>
-            </Paper>
-        </Container>
-    )
-}
+  return (
+    <Container maxWidth="xl">
+      <Paper elevation={4} className="p-4" style={{ marginTop: "16em" }}>
+        <h2>This is a Landing page!</h2>
+        <h1>Landing here</h1>
+      </Paper>
+    </Container>
+  );
+};
 
 export default LandingPage;
